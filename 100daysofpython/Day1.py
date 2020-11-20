e32710c2fec6bc21c762b368ab6041a7d2636dd1
Jeremy Kosse <jkosse@imagineps.com>
2020-11-20 11:57:17 -0600

    Added day 1

---
+++ 100daysofpython/Day1.py
@@ -0,0 +1,17 @@
+# 100 Days of Python!
+# Jeremy Kosse
+# 11/20/2020
+# Day 1: Working with Variables in Python to Manage Data
+
+# Intro
+print("*" * 80)
+print("Welcome to Day 1 of my personal 100 days of coding exercises!")
+print("BAND NAME GENERATOR!!!")
+print("*" * 80)
+
+# Collect data
+city_name = input("Which city did you grow up in? ")
+pet_name = input("What is your pet's name? ")
+
+# Process data into desired output
+print("Your band name is {} {}".format(city_name,pet_name))
